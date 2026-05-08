487e25e7698fb7f599475a748ded5ef5e0dee281
kaneflores <kaneflores911@gmail.com>
2026-05-08 15:09:22 -0400

    cmd line arguments

--- embed.c
+++ embed.c
@@ -1,6 +1,17 @@
 /* c.c */
 #include "embed.h"
 
+void usage(int8 &arg){
+    fprintf(stderr, "Usage: %s [-h | --help | -a] <identifier>\n", $c arg);
+    exit(-1);
+}
 int main(int argc, char *argv[]) {
-    return 0;
+    if (argc >2) {
+        if (compare($1 argv[1], $1 "-h")
+            || (compare($1 argv[1], $1 "--help"))
+                usage($ argv[0]));
+        if (argc == 3) {
+
+        }
+    }
 }
--- embed.h
+++ embed.h
@@ -7,13 +7,13 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <errno.h>
-#include <birchutils.h>
+#include </birchutils.h>
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
-typedef unsigned _BitInt(128);
+typedef unsigned _BitInt(128) int128;
 
 #define $8 (int8 *)
 #define $6 (int16)
